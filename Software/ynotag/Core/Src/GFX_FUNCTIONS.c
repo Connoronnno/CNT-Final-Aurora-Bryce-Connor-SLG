25b65528af2bd2dcc85abb35bb7a4829f25710ed
Connor Marsh <cmarsh7@nait.ca>
2025-03-11 15:45:31 -0600

    do not use this commit

--- Software/ynotag/Core/Src/GFX_FUNCTIONS.c
+++ Software/ynotag/Core/Src/GFX_FUNCTIONS.c
@@ -22,7 +22,7 @@ extern uint8_t _ystart;
 
 void drawPixel(int16_t x, int16_t y, uint16_t color)
 {
-	ST7735_DrawPixel(y, x, color);
+	ST7735_DrawPixel(x, y, color);
 }
 
 void fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t color)
@@ -394,6 +394,25 @@ void fillScreen(uint16_t color) {
     fillRect(0, 0, _width, _height, color);
 }
 
+void drawImage(uint16_t image[][2], uint16_t palette[], uint16_t x, uint16_t y, uint16_t w, uint16_t h)
+{
+	uint16_t i;
+	uint16_t j;
+	uint16_t totalInd =0;
+	uint16_t ind;
+	uint16_t count;
+	uint16_t buffer[w*h];
+for(i=0; i<332; i++)
+{
+	ind = image[i][0];
+	count = image[i][1];
+	for(j=0; j<count; j++)
+	{
+		buffer[totalInd++] = palette[ind];
+	}
+}
+ST7735_DrawImage(x, y, w, h, buffer);
+}
 
 
 void testLines(uint16_t color)
