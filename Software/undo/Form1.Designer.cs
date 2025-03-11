d59500f705929416c2ca9ec5e14e0932d9330d67
Connor Marsh <cmarsh7@nait.ca>
2025-03-11 15:48:34 -0600

    c# bmp proc essing

---
+++ Software/undo/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace undo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this._process = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 113);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(437, 220);
+            this.textBox1.TabIndex = 0;
+            // 
+            // _process
+            // 
+            this._process.Location = new System.Drawing.Point(370, 42);
+            this._process.Name = "_process";
+            this._process.Size = new System.Drawing.Size(75, 23);
+            this._process.TabIndex = 1;
+            this._process.Text = "button1";
+            this._process.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this._process);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button _process;
+    }
+}
+
---
+++ Software/undo/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace undo
+{
+    public partial class Form1 : Form
+    {
+        Bitmap bm;
+        List<Color> Pallette = new List<Color>();
+        List<string> a = new List<string>();
+        List<string> b = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+            AllowDrop = true;
+            DragEnter += Form1_DragEnter;
+            DragDrop += Form1_DragDrop;
+            _process.Click += _process_Click;
+        }
+
+        private void _process_Click(object sender, EventArgs e)
+        {
+            a = new List<string>();
+            b = new List<string>();
+            char count = (char)0;
+            char found = (char)0;
+            char i = (char)0;
+            if (bm != null)
+            {
+                for (int y = 0; y < bm.Height; y++)
+                {
+                    for (int x = 0; x < bm.Width; x++)
+                    {
+                        if (!Pallette.Contains(bm.GetPixel(x, y))) Pallette.Add(bm.GetPixel(x, y));
+                    }
+                }
+                for (int y = 0; y < bm.Height; y++)
+                {
+                    for (int x = 0; x < bm.Width; x++)
+                    {
+                        
+                        i = (char)0;
+                        foreach(Color c in Pallette) 
+                        {
+                            if (c == bm.GetPixel(x, y)) break;
+                            i++;
+                        }
+                        //b.Add($"{(int)i}, ");
+                        if (found != i)
+                        {
+                            found = i;
+                            a.Add($"{{{(int)found}, {((int)count)+1}}}, ");
+                            count = (char)0;
+                        }
+                        else 
+                        {
+                            count++;
+                        }
+                    }
+                }
+                foreach (string pixels in a) 
+                {
+                    textBox1.AppendText(pixels);
+                }
+                foreach (Color pixels in Pallette)
+                {
+                    textBox1.AppendText(pixels);
+                }
+            }
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.None;
+
+        }
+
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            //get file path
+            string path = ((string[])e.Data.GetData(DataFormats.FileDrop)).First();
+
+            //assign and display bitmap
+            bm = (Bitmap)Bitmap.FromFile(path);
+        }
+    }
+}
